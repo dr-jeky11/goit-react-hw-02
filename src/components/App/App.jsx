00df597947cd558353ab2060d009eed5acbb2511
dr-jeky11 <tyrsyna@gmail.com>
2024-07-02 12:20:03 +0200

    hw 2 setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,5 @@
-import userData from '../../userData.json'
-import friends from '../../friends.json'
-import transactions from '../../transactions.json'
-
-import Profile from '../Profile/Profile';
-import FriendList from '../FriendList/FriendList';
-import TransactionHistory from '../TransactionHistory/TransactionHistory'
-
 const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  return <h1>Just do it!</h1>;
 };
 
 export default App;
